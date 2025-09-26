575abc838c395029cc26b35bb11bd589dfc8b398
[dyad] <juansaucortes@gmail.com>
2025-09-26 15:41:22 -0600

    [dyad] Reposicionando el cuadro de redes sociales - wrote 2 file(s)

--- src/components/Slideshow.tsx
+++ src/components/Slideshow.tsx
@@ -3,7 +3,7 @@ import useEmblaCarousel from 'embla-carousel-react';
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
-import SocialMediaBox from './SocialMediaBox'; // Importa el nuevo componente
+// Eliminado: import SocialMediaBox from './SocialMediaBox';
 
 interface Slide {
   id: number;
@@ -96,11 +96,7 @@ const Slideshow = () => {
         </div>
       </div>
 
-      {/* SocialMediaBox posicionado al centro y por encima */}
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20">
-        <SocialMediaBox />
-      </div>
-
+      {/* Los botones de navegación del slideshow */}
       <div className="absolute bottom-8 left-1/2 -translate-x-1/2 flex space-x-4 z-10">
         <Button
           variant="outline"
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,17 @@
 import Navbar from "@/components/Navbar";
-import Slideshow from "@/components/Slideshow"; // Importa el nuevo componente Slideshow
+import Slideshow from "@/components/Slideshow";
+import SocialMediaBox from "@/components/SocialMediaBox"; // Importa el SocialMediaBox
 
 const Index = () => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col relative"> {/* Añadido 'relative' para posicionamiento absoluto */}
       <Navbar />
-      <Slideshow /> {/* Renderiza el Slideshow debajo del Navbar */}
-      {/* El contenido de bienvenida ha sido eliminado */}
+      <Slideshow />
+      
+      {/* SocialMediaBox posicionado a la derecha y centrado verticalmente */}
+      <div className="absolute right-0 top-1/2 -translate-y-1/2 z-20">
+        <SocialMediaBox />
+      </div>
     </div>
   );
 };
