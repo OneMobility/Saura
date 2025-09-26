17422ecc5c82c1968a6e5f50a53cabaf76fb8690
[dyad] <juansaucortes@gmail.com>
2025-09-26 15:35:54 -0600

    [dyad] Actualizando el nombre de la aplicación y añadiendo un icono de palmera en la navbar - wrote 1 file(s)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PalmTree } from 'lucide-react'; // Importa el icono de palmera
 
 const Navbar = () => {
   return (
@@ -8,7 +9,10 @@ const Navbar = () => {
         className="bg-rosa-mexicano flex items-center pl-4 pr-12"
         style={{ clipPath: 'polygon(0 0, 100% 0, 85% 100%, 0 100%)', minWidth: '200px' }}
       >
-        <div className="text-lg font-bold text-white">Mi Aplicación</div>
+        <div className="flex items-center space-x-2"> {/* Contenedor para el icono y el texto */}
+          <PalmTree className="text-white h-6 w-6" /> {/* Icono de palmera */}
+          <div className="text-lg font-bold text-white">Saura Tours</div>
+        </div>
       </div>
 
       {/* Enlaces de navegación */}
